605c7336aaa489475d733267f871f0daa469b3c9
dashpushaws <dashpush.aws@gmail.com>
2021-02-17 14:40:19 +0900

    mountain app

---
+++ shared/MountainList.js
@@ -0,0 +1,38 @@
+export const LISTDATA = [
+  {
+    id: 1,
+    title: '설악산',
+    image: "https://picsum.photos/id/1/300/300",
+    subtitle: 'aaaaaaaaaaaaaa',
+    description:'11111111111111111111111111111'
+  },
+  {
+    id: 2,
+    title: '북한산',
+    image: "https://picsum.photos/id/20/300/300",
+    subtitle: 'bbbbbbbbbbbbb',
+    description:'222222222222222222222222222222'
+  },
+  {
+    id: 3,
+    title: '천마산',
+    image: "https://picsum.photos/id/200/300/300",
+    subtitle: 'cccccccccccc',
+    description:'333333333333333333333333333333'
+  },
+  {
+    id: 4,
+    title: '지리산',
+    image: "https://picsum.photos/id/1000/300/300",
+    subtitle: 'dddddddddddd',
+    description:'4444444444444444444444444444444444444'
+  },
+  {
+    id: 5,
+    title: '한라산',
+    image: "https://picsum.photos/id/1000/300/300",
+    subtitle: 'eeeeeeeeeeeeee',
+    description:'555555555555555555555555555555555555555555555555555'
+  },
+
+]
